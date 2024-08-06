79670d482b498e955bf608b18d73e961db9bf4d7
ulibdev <ulisesbdeveloper@gmail.com>
2024-08-06 19:14:41 -0300

    four

---
+++ 4MathematicHypotenuse/Untitled4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+	float a,b,hip;
+	
+	cout<<"A: ";cin>>a;
+	
+	cout<<"B: ";cin>>b;
+	
+	
+	hip = sqrt(pow(a,2) +pow(b,2));
+	
+	
+	cout<<"Hypotenuse is "<<hip<<endl;
+	
+	
+	return 0;
+}
+
+
+
+
+
