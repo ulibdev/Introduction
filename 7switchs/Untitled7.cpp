d577b9303cf4ea353b5a39a6da24672fd288bd53
ulibdev <ulisesbdeveloper@gmail.com>
2024-08-06 19:41:42 -0300

    .

---
+++ 7switchs/Untitled7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+int main(){
+	
+	int a;
+	
+	std::cout<<"A:";std::cin>>a;
+	
+	switch(a){
+		
+		case 1:
+			std::cout<<"is 1";
+			break;
+		case 2:
+			std::cout<<"is 2";
+			break;
+		default:
+			std::cout<<"Error";
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
